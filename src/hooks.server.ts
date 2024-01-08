abb8d591607b2464fe9d20387df3746c1d7b2ed7
Jamey Smith <smith.jameyd@gmail.com>
2024-01-08 11:46:38 +0100

    feat: add session config, rename prisma to server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,16 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "$lib/prisma.server";
 
 export const handle = SvelteKitAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "database",
+    generateSessionToken: () => {
+      return crypto.randomUUID(); // generate a uuid
+    },
+  },
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
 });
