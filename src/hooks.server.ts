ec816c4f151cc27c5200a45a1647554db1c9989d
Jamey Smith <smith.jameyd@gmail.com>
2024-01-08 11:09:29 +0100

    fix: remove prisma adapter for testing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,7 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
-import prisma from "$lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const handle = SvelteKitAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
 });
